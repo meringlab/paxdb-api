ff1c8082b92e176f5dc3414f60699b757338314e
Milan Simonovic <mbsimonovic@gmail.com>
2016-08-19 15:08:52 +0200

    add multiple proteins api
    Former-commit-id: a2d2f3fac18ec477b7082c741e29794d47318401 [formerly 783b6b8974eb204638167f051153acd9d22c8f23]
    Former-commit-id: b811f39b2ceff66422edaa529cee8af7ed0c1da9

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var routes = require('./routes/index');
 var species = require('./routes/species');
 var dataset = require('./routes/dataset');
 var protein = require('./routes/protein');
+var proteins = require('./routes/proteins');
 
 var app = express();
 
@@ -16,6 +17,7 @@ app.use('/', routes);
 app.use('/species', species);
 app.use('/dataset', dataset);
 app.use('/protein', protein);
+app.use('/proteins', proteins);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/proteins.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const router = express.Router();
+const bunyan = require('bunyan');
+
+const speciesData = require('../lib/species');
+const proteinData = require('../lib/proteins');
+const datasetData = require('../lib/dataset_data');
+const datasetLib = require('../lib/dataset');
+
+const speciesForProtein = {}
+
+for (var speciesId in proteinData) {
+  for (var proteinId in proteinData[speciesId]) {
+    speciesForProtein[proteinId] = speciesId;
+  }
+}
+
+const log = bunyan.createLogger({
+  name: "paxdb-API",
+  module: "proteins"
+  //TODO server / host / process ..
+});
+
+router.get('/', (req, res) => {
+  var ids = req.query.ids.split(',').map(i => {return parseInt(i, 10)});
+  for (var i = 0; i < ids.length; i++) {
+    if (!(ids[i] in speciesForProtein)) {
+      res.status(404);
+      res.render('error', { message: `Unknown protein: ${id}` });
+      return;
+    }
+  }
+  var speciesId = speciesForProtein[ids[0]];//all belong to the same
+
+  var proteins = ids.map(function(id) {
+    var protein = proteinData[speciesId][id];
+    var rec = {id: protein.id, name: protein.name};
+    rec.abundances = speciesData[speciesId].datasets.map(function(datasetInfo) {
+      const abundance = datasetData.abundances[datasetInfo.id][id];
+      return datasetLib.formattedAbundance(abundance ? abundance.a : null);
+    })
+    return rec;
+  });
+  const result = { proteins, datasets: speciesData[speciesId].datasets };
+
+  res.header('content-type', 'application/json');
+  res.send(JSON.stringify(result))
+});
+
+module.exports = router;
+//TODO
+//exports = module.exports = function (options) {
+//    return router
+//}
