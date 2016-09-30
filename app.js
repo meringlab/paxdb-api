1370bd8f95d976c3aaf1b1956a69d1687dc947de
Milan Simonovic <mbsimonovic@gmail.com>
2016-09-30 20:40:19 +0200

    bugfix: render error view

--- app.js
+++ app.js
@@ -45,8 +45,7 @@ if (app.get('env') === 'development') {
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
-    message: err.message,
-    error: {}
+    message: err.message
   });
 });
 
