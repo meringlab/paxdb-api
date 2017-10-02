340e178e7939bdd963de5b08c06d81276d290341
Milan Simonovic <mbsimonovic@gmail.com>
2017-10-02 09:33:00 +0200

    make linter happy

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-var express = require('express');
-var path = require('path');
+const express = require('express');
+const path = require('path');
 
-var routes = require('./routes/index');
-var species = require('./routes/species');
-var dataset = require('./routes/dataset');
-var protein = require('./routes/protein');
-var proteins = require('./routes/proteins');
+const routes = require('./routes/index');
+const species = require('./routes/species');
+const dataset = require('./routes/dataset');
+const protein = require('./routes/protein');
+const proteins = require('./routes/proteins');
 
 const logger = require('bunyan').createLogger({
-  name: "paxdb-API",
-  module: "app"
+  name: 'paxdb-API',
+  module: 'app'
   //TODO server / host / process ..
 });
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -26,8 +26,8 @@ app.use('/protein', protein);
 app.use('/proteins', proteins);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
@@ -37,7 +37,7 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
+  app.use((err, req, res) => {
     res.status(err.status || 500);
     res.render('error', {
       message: err.message,
@@ -48,7 +48,7 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use((err, req, res) => {
   res.status(err.status || 500);
   res.render('error', {
     message: err.message
