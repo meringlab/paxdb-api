d24c3f68e68b21466c55f7be576530e7ca3c3b57
Milan Simonovic <mbsimonovic@gmail.com>
2019-02-20 15:28:15 +0100

    add a unit test

--- test/unit.js
+++ test/unit.js
@@ -1,8 +1,9 @@
 const { expect, assert } = require('chai');
 const should = require('should');
 const datasetLib = require('../lib/dataset');
+const scatter = require('../lib/scatter');
 
-describe("Ranking", function () {
+describe("Ranking", function() {
     const ranking = new datasetLib.Ranking(100);
 
     describe("relative ranking", function () {
@@ -19,12 +20,32 @@ describe("Ranking", function () {
         it('should be empty for anything between top and bottom', function () {
             [26,27, 40, 50, 60, 70, 73, 74].forEach(val => expect(ranking.relativeRank(val)).to.be.eql('',val));
         });
-        it('should handle missing abundances', function () {
-            ranking.relativeRank(null).should.eql('');
-            ranking.relativeRank(undefined).should.eql('');
+        it('should handle missing abundances', function() {
+            ranking.relativeRank(null)
+                .should
+                .eql('');
+            ranking.relativeRank(undefined)
+                .should
+                .eql('');
 
             // ranking.absoluteRank(null).should.eql('');
             // ranking.absoluteRank(undefined).should.eql('');
+        });
+    });
+});
+
+describe("Scatter plots", function() {
+    it('should correlate two datasets', function() {
+        let d1 = { "1": { "a": 807, "r": 2 }, "2": { "a": 9.55, "r": 3 }, "3": { "a": 1555.1, "r": 1 } };
+        let d2 = { "1": { "a": 39.2, "r": 2 }, "2": { "a": 22.1, "r": 3 }, "3": { "a": 67, "r": 1 } };
+        let groups = [{ members: [5, 6] }, { members: [1, 16] }, { members: [2, 3] }, { members: [3, 16] }];
+
+        let expected = [[807, 39.2], [9.55 + 1555.1, 22.1 + 67], [1555.1, 67]]
+            .map(e => [Math.log10(e[0]), Math.log10(e[1])]);
+        let correlation = scatter.correlate(d1, d2, groups);
+        expected.forEach((e,i) => {
+            e[0].should.eql(correlation[i][0], 0.01);
+            e[1].should.eql(correlation[i][1], 0.01);
         });
     });
 });
