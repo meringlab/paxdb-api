06e4110a07a4759981dda08bb4bf8cebaf8096d2
qingyao <qingy.huang@gmail.com>
2023-03-22 17:42:19 +0100

    update 'proteins' route for changed protein ID & deleted speciesForProtein object

--- routes/proteins.js
+++ routes/proteins.js
@@ -9,7 +9,6 @@ const datasetLib = require('../lib/dataset');
 const proteinIndices = require('../lib/proteins_index');
 
 const router = new express.Router();
-const { speciesForProtein } = proteinIndices;
 
 const log = bunyan.createLogger({
     name: 'paxdb-API',
@@ -19,39 +18,33 @@ const log = bunyan.createLogger({
 
 router.get('/', (req, res) => {
     log.info({ query: req.query.ids });
-    const ids = req.query.ids.split(',')
-        .map(i => parseInt(i, 10));
+    const ids = req.query.ids.split(','); //.map(i => parseInt(i, 10))
     if (ids.length === 0) {
         res.status(400);
         res.set('Content-type', 'application/json');
         res.render('error', { message: 'No proteins found!' });
         return;
     }
+    
     for (let i = 0; i < ids.length; i += 1) {
-        if (!(ids[i] in speciesForProtein)) {
+        if ( i == 0) {
+            speciesId = ids[i].split('.')[0];
+        } else if (speciesId !== ids[i].split('.')[0]) {
             res.status(404);
-            res.set('Content-type', 'application/json');
-            res.render('error', { message: `Unknown protein: ${ids[i]}` });
-            return;
-        }
-    }
-    const speciesId = speciesForProtein[ids[0]];//all belong to the same
-    for (let i = 0; i < ids.length; i += 1) {
-        if (speciesId !== speciesForProtein[ids[i]]) {
-            res.status(400);
             res.set('Content-type', 'application/json');
             res.render('error', { message: 'All proteins must belong to the same species!' });
             return;
         }
     }
-
+    
     const proteinsMap = require(`../lib/proteins/${speciesId}`);
     const proteins = ids.map((id) => {
-        const protein = proteinsMap[id];
+        const short_id = id.split('.')[1];
+        const protein = proteinsMap[short_id];
         const rec = { id: protein.id, externalId: protein.externalId, name: protein.name };
         rec.abundances = speciesData[speciesId].datasets.map((datasetInfo) => {
             const dataset = require(`../lib/dataset/${datasetInfo.id}`);
-            const abundance = dataset.abundances[id];
+            const abundance = dataset.abundances[short_id];
             return datasetLib.formattedAbundance(abundance ? abundance.a : null);
         });
         return rec;
